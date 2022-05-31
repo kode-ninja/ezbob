c5e6ac3b31b5d02abddd167102928865de591581
alon <alon@ebby.co>
2022-05-31 13:37:09 +0300

    removed console.log

--- src/history/useSearchHistory.js
+++ src/history/useSearchHistory.js
@@ -4,18 +4,15 @@ const useSearchHistory = () => {
     const [searchHistory, setSearchHistory] = useState([]);
 
     const push = title => {
-        console.log('useSearchHistory.push(' + title + ')')
         remove(title);
         setSearchHistory(currHistory => [title, ...currHistory]);
     }
 
     const remove = title => {
-        console.log('useSearchHistory.remove(' + title + ')')
         setSearchHistory(currHistory => currHistory.filter(historyTitle => historyTitle !== title));
     }
 
     const exists = title => {
-        // console.log('useSearchHistory.exists(' + title + ')')
         return searchHistory.includes(title);
     }
 
