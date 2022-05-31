f2a58324027ce0931f3744a135c4cd30bd56cc63
alon <alon@ebby.co>
2022-05-31 16:11:38 +0300

    SuggestionTitle styling in now is App.css
    installed classnames package

--- src/components/search/input/suggestions/suggestion/SuggestionTitle.js
+++ src/components/search/input/suggestions/suggestion/SuggestionTitle.js
@@ -1,12 +1,12 @@
 import React, {useContext} from "react";
 import {SuggestionContext} from "./Suggestion";
+import classNames from "classnames";
 
 const SuggestionTitle = () => {
     const suggestion = useContext(SuggestionContext);
-    const style = suggestion.isExistsInSearchHistory ? {'color': 'purple'} : {};   // TODO: convert to class
 
     return (
-        <span style={style}>
+        <span className={classNames('title', {'in-history': suggestion.isExistsInSearchHistory})}>
             {suggestion.title}
         </span>
     );
