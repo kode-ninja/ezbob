34d332981979c09f1bf247e9ca9195abfb0b27d3
alon <alon@ebby.co>
2022-05-31 11:44:20 +0300

    comment improved

--- src/components/search/input/suggestions/Suggestions.js
+++ src/components/search/input/suggestions/Suggestions.js
@@ -6,11 +6,11 @@ import {useMemo} from "react";
 const MAX_NUM_OF_SUGGESTIONS = 10;
 
 const Suggestions = ({searchTerm, isToShowSuggestions}) => {
-    // Memoize suggestion calculation to prevent re-calculation (or a call to the DB)
-    // if input gains focus again.
+    // Memoize suggestion calculation to prevent
+    // re-calculation (DB/Server call) if input gains focus again.
     // Memoization depends on the search term - if it doesn't change, no need to re-calculate the suggestions
     const suggestions = useMemo(() => {
-        console.log('RECALCULATING')
+        console.log('Suggestions are being recalculated!');
         const suggestedTitles = getTitlesByPrefix(searchTerm, MAX_NUM_OF_SUGGESTIONS);
         return suggestedTitles.map((suggestedTitle, idx) => <Suggestion key={idx} title={suggestedTitle}/>);    // TODO: provide meaningful "key" or is it better, performance wise, to teardown and recreate the tree on render?
     }, [searchTerm]);
